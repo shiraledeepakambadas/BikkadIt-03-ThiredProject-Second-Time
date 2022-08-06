ef3604cd2d57fdcb8e0db003a28927948da97cd8
shiraledeepakambadas <shiraledeepak36@gmail.com>
2022-08-06 15:52:13 +0530

    gita id:git thired project first  commit

---
+++ src/main/java/com/example/demo/BikadIt03ThiredProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikadIt03ThiredProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikadIt03ThiredProjectApplication.class, args);
+	}
+
+}
